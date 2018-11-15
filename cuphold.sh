79715a5c3a1fdab15e208fd24b4e29e8201f1680
inkybo <inkybo@gmail.com>
2018-11-15 17:39:05 -0500

    adding sweetner

---
+++ cuphold.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+while getopts ":cdl" option;
+do
+  case $option in
+    c)
+    cat carl
+    exit 0
+    ;;
+    d)
+    cat tutor
+    exit 0
+    ;;
+  esac
+done
+cat cd_tray_image
+eject
